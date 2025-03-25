7f0fb90aa1238d188a053c2cbebcf97228abec37
Garima Bhayana <garimaabhayanaa@gmail.com>
2025-03-26 02:02:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://cocreatecolab.netlify.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
